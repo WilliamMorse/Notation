c77aaf4987f27414c1b4470b9cd3e85732adc664
William Morse <william.b.morse@maine.edu>
2016-08-08 20:09:16 -0400

    added a reset bu

--- pr2.elm
+++ pr2.elm
@@ -16,7 +16,7 @@ model = -- start the counter at zero
   0
 
 -- UPDATE
-type Msg = Increment | Decrement -- two messages possible we will do something diferent for each
+type Msg = Increment | Decrement | Reset -- two messages possible we will do something diferent for each
 
 update : Msg -> Model -> Model -- no idea what this structure is....
 update msg model =
@@ -25,6 +25,8 @@ update msg model =
       model + 1 -- add one to the model which is just a counter
     Decrement -> -- if we get a dec message
       model - 1 -- subtract from the model
+    Reset ->
+      0 -- set the model to the innitial value
 
 -- VIEW
 
@@ -34,4 +36,5 @@ view model =
     [ button [onClick Decrement ] [ text "Decrement"] -- define the message to send to the update function
     , div [] [ text (toString model) ] -- use the inbult toString function to change type from number to string
     , button [ onClick Increment ] [text "Increment"] -- onClick is the function that gets called when we click the button?
+    , button [ onClick Reset ] [text "Reset"] -- add a reset button to the view
     ]
